8f245505c3eb3e9c632bc588b2af42fcaa6ebf6d
Tim Mitra <tim@it-guy.com>
2024-01-07 20:26:15 -0500

    Initial Commit

---
+++ RevenueCatDemo/RevenueCatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevenueCatDemoApp.swift
+//  RevenueCatDemo
+//
+//  Created by Tim Mitra on 2024-01-07.
+//
+
+import SwiftUI
+
+@main
+struct RevenueCatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
