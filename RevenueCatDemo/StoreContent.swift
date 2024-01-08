044bd54b03611330ee5948b5eba627ebd358727a
Tim Mitra <twitter@it-guy.com>
2024-01-08 13:11:42 -0500

    add more purchase page

---
+++ RevenueCatDemo/StoreContent.swift
@@ -0,0 +1,18 @@
+//
+//  StoreContent.swift
+//  RevenueCatDemo
+//
+//  Created by Tim Mitra on 1/8/24.
+//
+
+import SwiftUI
+
+struct StoreContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreContent()
+}
