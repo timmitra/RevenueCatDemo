f0dd79cee3199596a3341231ac8a08d9972078a7
Tim Mitra <tim@it-guy.com>
2024-01-09 09:36:26 -0500

    trying to use UIApplicationDelegate

--- RevenueCatDemo/RevenueCatDemoApp.swift
+++ RevenueCatDemo/RevenueCatDemoApp.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 import RevenueCat
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    return true
+  }
+}
+
 @main
 struct RevenueCatDemoApp: App {
+  
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
   
   init() {
     Purchases.configure(withAPIKey: "appl_LaYRANqhDmAdniTnaPZGfRIYABl")
@@ -21,4 +29,10 @@ struct RevenueCatDemoApp: App {
             ContentView()
         }
     }
+}
+
+extension AppDelegate: PurchasesDelegate {
+  func purchases(_ purchases: Purchases, receivedUpdated customerInfo: CustomerInfo) {
+    // handle changes to customer info
+  }
 }
